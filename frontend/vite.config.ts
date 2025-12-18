f5b2aaab52848c8ef0b5790892d777604a28e31c
cconner57 <chris.conner57@gmail.com>
2025-12-18 13:39:48 -0800

    feat: update Vite config to set default DNS result order and change API URL fallback

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dns from 'node:dns'
+
+dns.setDefaultResultOrder('ipv4first')
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
+
   return {
     plugins: [vue()],
     resolve: {
@@ -13,9 +17,8 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       proxy: {
-        // Any request starting with /v1 will be sent to the Backend
         '/v1': {
-          target: env.VITE_API_URL || 'http://localhost:8080', // Fallback to localhost if missing
+          target: env.VITE_API_URL || 'http://192.168.12.102:8080',
           changeOrigin: true,
           secure: false,
           rewrite: (path) => path.replace(/^\/v1/, ''),
